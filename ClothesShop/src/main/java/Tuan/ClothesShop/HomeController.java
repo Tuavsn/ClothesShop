fc39131d460c733d5c160b384c9115c675bce514
Tuavsn <hoctuavsn@gmail.com>
2024-01-13 23:32:07 +0700

    Start Project

---
+++ ClothesShop/src/main/java/Tuan/ClothesShop/HomeController.java
@@ -0,0 +1,12 @@
+package Tuan.ClothesShop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String homepage() {
+		return "index";
+	}
+}
