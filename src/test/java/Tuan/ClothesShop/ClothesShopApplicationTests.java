983be62f1fdcc28f0fe71a1e9c93bd880b948cb9
Tuavsn <43782459+Tuavsn@users.noreply.github.com>
2024-01-14 12:06:53 +0700

    Add files via upload

---
+++ src/test/java/Tuan/ClothesShop/ClothesShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Tuan.ClothesShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
